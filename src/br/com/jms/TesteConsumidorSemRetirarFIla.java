63c7e04f7c6fc2c42a9a9449cd36e179c2f379a1
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-17 19:08:43 -0300

    criando um consumidor sem retirar da fila

---
+++ src/br/com/jms/TesteConsumidorSemRetirarFIla.java
@@ -0,0 +1,49 @@
+package br.com.jms;
+
+import java.util.Enumeration;
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Queue;
+import javax.jms.QueueBrowser;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+import br.com.jms.util.PropertiesProducerJndi;
+
+public class TesteConsumidorSemRetirarFIla {
+	
+	@SuppressWarnings({ "resource", "rawtypes" })
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext(PropertiesProducerJndi.geraPropertiesMOM());
+		
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);//boolean sem transação
+		Destination fila = (Destination) context.lookup("financeiro");
+		
+		QueueBrowser browser = session.createBrowser((Queue) fila);
+		
+		Enumeration msgs = browser.getEnumeration();
+		
+		while(msgs.hasMoreElements()) {
+			TextMessage msg = (TextMessage) msgs.nextElement();
+			
+			System.out.println("Message: " + msg.getText());
+		}
+		
+		
+		new Scanner(System.in).hasNextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
